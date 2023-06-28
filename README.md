2b8b8eb87334bf158b4f59ad316af4991ded8475
anupshakya07 <anupshakya07@gmail.com>
2023-06-28 15:41:00 -0500

    Implemented Argparser for input parameters.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# verification
+# Verification
+
+## Dependencies List
+- Python v_3.8
+- DGL-cuda11.3 v_0.9.1
+- PyTorch v_1.12.1
+- scikit-learn v_1.2.2
+- scipy v_1.4.1
+
+## Run Command
+Execute the main.py with the appropriate parameters as follows:
+
+```
+ python main.py -num_nodes 1000 -num_clusters 50 -num_iters 30 -dataset "cora" -spec_model "gcn" -nuv_model "gat"
+```
+
+## Help
+To find more help for executing the code, enter the following:
+
+```
+python main.py --help
+```
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from hmln import CitationNetworkHMLN
 import utils
-
+import argparse
 
 def main():
-    num_nodes = 500
-    num_clusters = 50
-    num_iters = 30
-    dataset = "cora"
-    spec_model = "gcn"
-    nuv_model = "gat"
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-num_nodes", type=int, default=500, help="Number of nodes to take in training")
+    parser.add_argument("-num_clusters", type=int, default=50, help="Number of weight-sharing clusters to form")
+    parser.add_argument("-num_iters", type=int, default=30, help="Number of iterations to train the HMLN")
+    parser.add_argument("-dataset", type=str, default="cora", choices=["cora", "citeseer", "pubmed"],
+                        help="Choose the dataset name")
+    parser.add_argument("-spec_model", type=str, default="gcn", choices=["gcn", "gs", "gat"],
+                        help="Select the specification DNN.")
+    parser.add_argument("-nuv_model", type=str, default="gat",choices=["gcn", "gs", "gat"],
+                        help="Select the NUV DNN.")
+
+    opt = parser.parse_args()
+    num_nodes = opt.num_nodes
+    num_clusters = opt.num_clusters
+    num_iters = opt.num_iters
+    dataset = opt.dataset
+    spec_model = opt.spec_model
+    nuv_model = opt.nuv_model
 
     spec_embedding_filepath = f"embeddings/{dataset}/{spec_model}_specification_embeddings.pkl"
     spec_pred_filepath = f"embeddings/{dataset}/{spec_model}_specification_pred_prob.pkl"
